05d542160ae690b1a354d26273fcfa0c5a57b2b4
luchengcheng <luchengcheng@yujingceping.com>
2019-04-16 18:32:22 +0800

    添加eureka-server

---
+++ eureka-server/src/main/java/org/cheng/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package org.cheng.eureka.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class EurekaServerApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
