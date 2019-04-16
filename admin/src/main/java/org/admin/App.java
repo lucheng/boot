c957eb8dad8db92b099670e9f5e9dd58c08cb3d6
luchengcheng <luchengcheng@yujingceping.com>
2019-04-16 15:01:04 +0800

    不提交

---
+++ admin/src/main/java/org/admin/App.java
@@ -0,0 +1,13 @@
+package org.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
