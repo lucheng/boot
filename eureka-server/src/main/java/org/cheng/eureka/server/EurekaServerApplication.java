fdc3693a8423eebe8b203cec240d1691e0128dfe
luchengcheng <luchengcheng@yujingceping.com>
2019-04-17 12:13:04 +0800

    完善eureka-server

--- eureka-server/src/main/java/org/cheng/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/org/cheng/eureka/server/EurekaServerApplication.java
@@ -1,13 +1,13 @@
 package org.cheng.eureka.server;
 
-/**
- * Hello world!
- *
- */
-public class EurekaServerApplication 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
 }
