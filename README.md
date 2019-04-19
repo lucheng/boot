e4ab77a6b06aa3fee32e936815f2d754e98857e1
luchengcheng <luchengcheng@yujingceping.com>
2019-04-19 18:22:28 +0800

    添加user模块

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
+
+
 ###boot项目为一个SpringCloud框架的demo
 
+
+####版本信息####
+
 - Spring Boot 版本： 2.1.4.RELEASE
-- Spring Cloud 版本：Greenwich.RELEASE
+- Spring Cloud 版本：Greenwich.RELEASE  #格林尼治 - 英国伦敦格林尼治区，位于伦敦东南、泰晤士河南岸#
 
+####模块信息####
 
-- ####eureka-server 为注册中心
-
-- ####config-server为配置中心
+- eureka-server 为注册中心
+- config-server为配置中心
 
 
--- eureka-server/src/main/java/org/cheng/eureka/server/config/listener/EurekaStateListener.java
+++ eureka-server/src/main/java/org/cheng/eureka/server/config/listener/EurekaStateListener.java
@@ -2,6 +2,8 @@ package org.cheng.eureka.server.config.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
 import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRegisteredEvent;
 import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRenewedEvent;
@@ -9,6 +11,8 @@ import org.springframework.cloud.netflix.eureka.server.event.EurekaRegistryAvail
 import org.springframework.cloud.netflix.eureka.server.event.EurekaServerStartedEvent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import com.netflix.appinfo.InstanceInfo;
@@ -16,12 +20,21 @@ import com.netflix.appinfo.InstanceInfo;
 @Component
 @ComponentScan
 public class EurekaStateListener {
-	private final static Logger logger = LoggerFactory.getLogger(EurekaStateListener.class);
-
+	private final static Logger LOGGER = LoggerFactory.getLogger(EurekaStateListener.class);
+	
+	@Value("${spring.mail.username}")
+	private String mailUsername;
+	
+	@Value("${spring.mail.properties.set-to}")
+	private String mailSetTo;
+	
+	@Autowired
+    private JavaMailSender jms;
+	
 	@EventListener(condition = "#event.replication==false")
 	public void listen(EurekaInstanceCanceledEvent event) {
 		String msg = "服务" + event.getAppName() + "\n" + event.getServerId() + "已下线";
-		logger.info(msg);
+		LOGGER.info(msg);
 		this.send(msg);
 	}
 
@@ -30,25 +43,40 @@ public class EurekaStateListener {
 		InstanceInfo instanceInfo = event.getInstanceInfo();
 		String msg = "服务" + instanceInfo.getAppName() + "\n" + instanceInfo.getHostName() + ":" + instanceInfo.getPort()
 				+ " \nip: " + instanceInfo.getIPAddr() + "进行注册";
-		logger.info(msg);
+		LOGGER.info(msg);
 		this.send(msg);
 	}
 
 	@EventListener
 	public void listen(EurekaInstanceRenewedEvent event) {
-		logger.info("服务{}进行续约", event.getServerId() + "  " + event.getAppName());
+		LOGGER.info("服务{}进行续约", event.getServerId() + "  " + event.getAppName());
 	}
 
 	@EventListener
 	public void listen(EurekaRegistryAvailableEvent event) {
-		logger.info("注册中心启动,{}", System.currentTimeMillis());
+		LOGGER.info("注册中心启动,{}", System.currentTimeMillis());
 	}
 
 	@EventListener
 	public void listen(EurekaServerStartedEvent event) {
-		logger.info("注册中心服务端启动,{}", System.currentTimeMillis());
+		LOGGER.info("注册中心服务端启动,{}", System.currentTimeMillis());
 	}
 
 	private void send(String msg) {
+		// 用于封装邮件信息的实例
+		SimpleMailMessage smm = new SimpleMailMessage();
+		// 由谁来发送邮件
+		smm.setFrom(mailUsername);
+		// 邮件主题
+		smm.setSubject("eureka-server");
+		// 邮件内容
+		smm.setText(msg);
+		// 接受邮件
+		smm.setTo(mailSetTo.split(","));
+		try {
+			jms.send(smm);
+		} catch (Exception e) {
+			LOGGER.info(msg + "错误", e);
+		}
 	}
 }
