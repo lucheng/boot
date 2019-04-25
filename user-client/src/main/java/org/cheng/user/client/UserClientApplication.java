b22c57140c5016136a9da9328094fc0c28f7be2a
luchengcheng <luchengcheng@yujingceping.com>
2019-04-25 18:36:04 +0800

    修改配置

--- user-client/src/main/java/org/cheng/user/client/UserClientApplication.java
+++ user-client/src/main/java/org/cheng/user/client/UserClientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.context.annotation.Bean;
 
@@ -17,6 +18,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableCircuitBreaker
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableTurbine
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
