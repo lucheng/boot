ebf9f20ec5037e4041379e0a06d1ecc158e0ca41
luchengcheng <luchengcheng@yujingceping.com>
2019-05-10 18:33:48 +0800

    111

---
+++ module/sharding/src/main/java/org/cheng/sharding/ShardingApplication.java
@@ -0,0 +1,13 @@
+package org.cheng.sharding;
+
+/**
+ * Hello world!
+ *
+ */
+public class ShardingApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
