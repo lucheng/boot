5ccd76f7f9748fb4a82cce9ccf4d4c910c92b1c5
luchengcheng <luchengcheng@yujingceping.com>
2019-05-17 19:12:12 +0800

    完善

---
+++ auth/oauth2-auth-server/src/main/java/com/example/config/OAuth2Configuration.java
@@ -0,0 +1,20 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2Configuration extends AuthorizationServerConfigurerAdapter {
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        //添加客户端信息
+        clients.inMemory()                  // 使用in-memory存储客户端信息
+                .withClient("client")       // client_id
+                .secret("secret")                   // client_secret
+                .authorizedGrantTypes("authorization_code")     // 该client允许的授权类型
+                .scopes("app");                     // 允许的授权范围
+    }
+}
--- auth/service-auth/src/main/java/org/cheng/boot/auth/AuthApplication.java
+++ auth/service-auth/src/main/java/org/cheng/boot/auth/AuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-//@EnableResourceServer
+@EnableResourceServer
 @EnableEurekaClient
 @SpringBootApplication
 public class AuthApplication {
