9359637d1d6eaf44d3fd69ad9b6374aaa6f91f7c
luCheng <1213136330@qq.com>
2019-05-25 22:20:16 +0800

    临时

--- upms/auth-provider/src/main/java/org/cheng/boot/auth/provider/service/feign/BaseUserAccountRemoteService.java
+++ upms/auth-provider/src/main/java/org/cheng/boot/auth/provider/service/feign/BaseUserAccountRemoteService.java
@@ -3,21 +3,19 @@ package org.cheng.boot.auth.provider.service.feign;
 import org.cheng.boot.common.core.model.ResultBody;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "base-provider", fallbackFactory = BaseUserAccountRemoteService.HystrixClientFallback.class)
 public interface BaseUserAccountRemoteService {
 	
-	@PostMapping("/account/localLogin") @RequestMapping(value = "/port/{id}", method = RequestMethod.GET)
-	public ResultBody<Object> port(@PathVariable("id") Integer id);
+	@PostMapping("/account/localLogin")
+	public ResultBody<Object> localLogin(@RequestParam(value = "username") String username);
 	
 	@Component
 	public class HystrixClientFallback implements BaseUserAccountRemoteService {
 		@Override
-		public ResultBody<Object> port(Integer id) {
+		public ResultBody<Object> localLogin(String username) {
 			return new ResultBody<Object>();
 		}
 	}
--- upms/auth-provider/src/main/java/org/cheng/boot/auth/provider/service/impl/UserDetailsServiceImpl.java
+++ upms/auth-provider/src/main/java/org/cheng/boot/auth/provider/service/impl/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package org.cheng.boot.auth.provider.service.impl;
 
 import org.cheng.boot.auth.provider.service.feign.BaseUserAccountRemoteService;
+import org.cheng.boot.common.core.model.ResultBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,11 +10,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Security用户信息获取实现类
  * 
  * @author Cheng
  */
+@Slf4j
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
@@ -25,6 +29,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		ResultBody<Object> resultBody = baseUserAccountRemoteService.localLogin(username);
+		log.info("loadUserByUsername",resultBody);
 		if ("forezp".equals(username)) {
 			return User.withUsername("forezp").password(passwordEncoder.encode("123456")).roles("USER").build();
 		} else if ("admin".equals(username)) {
